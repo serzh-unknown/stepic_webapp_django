93438b7965c232a9b35796645a434d59478c78d6
serzh.unknown <unknown.serzh@yandex.ru>
2016-05-16 02:30:31 +0300

    step 2

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -fs /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-#sudo ln -fs /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-#sudo /etc/init.d/gunicorn restart
+sudo ln -fs /home/box/etc/hello.py /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn/restart

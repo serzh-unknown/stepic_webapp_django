14ed36c21825208b831c66f3d055d4770bdf5f07
serzh.unknown <unknown.serzh@yandex.ru>
2016-05-16 02:14:11 +0300

    add hello.py

---
+++ hello.py
@@ -0,0 +1,13 @@
+def myapp(env, start_response):
+    status = "200 OK"
+    headers = [
+        ("Content-Type", "text/plain")
+    ]
+    keys = env.keys()
+    body = ""
+    for key in keys:
+        if body:
+            body += "\n"
+        body += key + "=" + env[key]
+    start_response(status, headers)
+    return [body]

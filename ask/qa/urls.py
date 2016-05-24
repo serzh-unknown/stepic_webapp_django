37e62da000cd762cc7f5fb05f55b5fccba664268
serzh.unknown <unknown.serzh@yandex.ru>
2016-05-24 03:56:19 +0300

    edit urls and add gunicorn.conf

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url
 
 urlpatterns = [
   url(r'^$', views.test, name='test'),
-  url(r'^login', views.test, name='test'),
-  url(r'^signup', views.test, name='test'),
-  url(r'^question/\d+', views.test, name='test'),
-  url(r'^ask', views.test, name='test'),
-  url(r'^popular', views.test, name='test'),
-  url(r'^new', views.test, name='test'),
+  url(r'^login/$', views.test, name='test'),
+  url(r'^signup/$', views.test, name='test'),
+  url(r'^question/(?P<pk>\d+)/$', views.test, name='test'),
+  url(r'^ask/$', views.test, name='test'),
+  url(r'^popular/$', views.test, name='test'),
+  url(r'^new/$', views.test, name='test'),
 ]

1bd86b80d0bd6f16c602862c4c1bca78fbeaeeaa
serzh.unknown <unknown.serzh@yandex.ru>
2016-05-24 05:03:05 +0300

    create models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Question(models.Model):
+  title = models.CharField(default="")
+  text = models.TextField(default="")
+  added_at = models.DateField(null=True)
+  rating = models.IntegerField(default=0)
+  author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+  likes = models.ManyToManyField(User, related_name="q_to_likes")
+
+  def __str__(self):
+    return self.title
+
+  def get_url(self):
+    return "/question/{}".format(self.id)
+
+
+class Answer(models.Model):
+  text = models.TextField(default="")
+  added_at = models.DateField(null=True)
+  question = models.ForeignKey(Question)
+  author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+  def __str__(self):
+    return self.text
